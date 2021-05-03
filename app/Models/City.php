c60ac9c48cc6cf493185b91502c6ba68529dba18
jlgregorio81 <jlgregorio81@gmail.com>
2021-05-03 20:38:21 -0300

    Starting with relationships in model

--- app/Models/City.php
+++ app/Models/City.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class City extends Model
 {
     use HasFactory;
+
+    public function clients(){
+        return $this->hasMany(Client::class);
+    }
+
 }
--- app/Models/Client.php
+++ app/Models/Client.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Client extends Model
 {
     use HasFactory;
+
+    //..eager loading 
+    protected $with = ['city', 'telephone'];
+
+    public function telephone(){
+        return $this->hasOne(Telephone::class, 'id', 'id');
+    }
+
+    public function delete(){
+        $this->telephone()->delete();
+        parent::delete();
+    }
+
+    public function city(){
+        return $this->belongsTo(City::class);
+    }
+
+
 }
